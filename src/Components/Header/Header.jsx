93fd499cbbaac8cefc2c6a6d3c15592f1f22cfc8
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-09-15 09:55:45 +0600

    Environment setup and Header section added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-[#1C1B1B] font-bold text-center mt-12 mb-8">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
